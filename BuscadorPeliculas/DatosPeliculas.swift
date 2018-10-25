8299e0757006bd25e9b4785f34722b9e6c0251a2
Alumno <ulsa@iMacD110.local>
2018-10-25 13:52:23 -0700

    Lista de peliculas

---
+++ BuscadorPeliculas/DatosPeliculas.swift
@@ -0,0 +1,9 @@
+//
+//  DatosPeliculas.swift
+//  BuscadorPeliculas
+//
+//  Created by Alumno on 25/10/18.
+//  Copyright © 2018 David. All rights reserved.
+//
+
+import Foundation
---
+++ BuscadorPeliculas/ListaPeliculasController.swift
@@ -0,0 +1,9 @@
+//
+//  ListaPeliculasController.swift
+//  BuscadorPeliculas
+//
+//  Created by Alumno on 25/10/18.
+//  Copyright © 2018 David. All rights reserved.
+//
+
+import Foundation
